d5badbc98ec0ffd25b7cd7e2ce4b80cd0a969539
Davidmous <117893895+Davidmous@users.noreply.github.com>
2022-11-24 15:37:17 +0700

    Add files via upload

---
+++ Soal1_C_71220909.py
@@ -0,0 +1,23 @@
+print("Menu")
+print("1.Tambah")
+print("2.Kurang")
+print("3.Kali")
+print("4.Bagi")
+a=int(input("Masukan Angka Pertama :"))
+b=int(input("Masukan Angka Kedua :"))
+Menu= input("Pilihan Anda :")
+if Menu=="1" :
+    Tambah=a+b
+    print("Hasil",Tambah)
+elif Menu=="2" :
+    Tambah=a-b
+    print("Hasil",Kurang)
+elif Menu=="3" :
+    Tambah=a*b
+    print("Hasil",Kali)
+elif Menu=="4" :
+    Tambah=a//b
+    print("Hasil",Bagi)
+
+
+#DavidDelonSeay
